9612e1ab15cb4f8988b6170d79b81641759336a5
JosuaKugler <josua.kugler@gmail.com>
2020-03-18 12:50:49 +0100

    basic opening works

--- tagger.hs
+++ tagger.hs
@@ -5,7 +5,9 @@ import qualified Data.ByteString.Lazy as B
 import System.Environment
 import System.IO
 import System.Directory
+import System.Posix.Files
 import Data.List.Split
+import Data.List
 
 jsonFile :: FilePath
 jsonFile = "/home/josua/repos/tagger/tagger.json"
@@ -31,16 +33,34 @@ instance FromJSON File
 getJSON :: IO B.ByteString
 getJSON = B.readFile jsonFile
 
+matchIndex :: File -> [String] -> ([Bool], String)
+matchIndex (File {fileName = fname, tags = ftags}) tags = ((map (`elem` tags) ftags), fname)
+
+getRanking :: [File] -> [String] -> [([Bool], String)]
+getRanking content tags = sortBy sortR $ foldl (\acc file -> (matchIndex file tags) : acc ) [] content
+
+compareBools :: [Bool] -> [Bool] -> Ordering
+compareBools [] _ = EQ
+compareBools _ [] = EQ
+compareBools a b = let res = compare (head b) (head a) 
+    in if res == EQ then compareBools (tail a) (tail b) else res
+
+sortR :: ([Bool], String) -> ([Bool], String) -> Ordering --first: more tags, second: earlier tags
+sortR (a1,b1) (a2,b2) = 
+    let sum = foldl (\acc bool -> if bool then acc + 1 else acc) 0 in
+    if (sum a1) /= (sum a2) then compare (sum a2) (sum a1) else compareBools a1 a2
+
+displayRanking :: [([Bool], String)] -> String
+displayRanking ranking = let withNumberRanking = zip [0..] ranking
+    in unlines $ map displayOneRanking withNumberRanking
+    where displayOneRanking (number, entry) = (show number) ++ ".: " ++ (snd entry) ++ "   " ++ (show $ fst entry)
 
 save fileargs = do
     let fileName = head $ reverse $ splitOn "/" $ head fileargs --get pure filename without path
     d <- (eitherDecode <$> getJSON) :: IO (Either String [File])
     case d of 
-        Left err -> let prev = if err == "Error in $: not enough input" then Right [] else Left err
-        Right a  -> let prev = Right a
-    case prev of
-        Left err    -> putStrLn err
-        Right prev  -> do 
+        Left err -> putStrLn err
+        Right prev -> do
             renameFile (head fileargs) (internalDirectory ++ "/" ++ fileName) --mv to internal dir
             let newfile = File {fileName = fileName, tags = tail fileargs}
             let next = newfile : prev
@@ -50,12 +70,13 @@ open fileargs = do
     d <- (eitherDecode <$> getJSON) :: IO (Either String [File])
     case d of 
         Left err -> putStrLn err
-        Right prev -> do
-            let newfile = File {fileName = (head fileargs), tags = tail fileargs}
-            let next = newfile : prev
-            B.writeFile jsonFile (encode next)
-
-
+        Right content -> do 
+            let ranking = getRanking content fileargs 
+            putStrLn $ displayRanking ranking
+            putStrLn "Enter a number to select a file"
+            n <- getLine
+            let fileName = snd (ranking !! (read n))
+            createSymbolicLink (internalDirectory ++ "/" ++ fileName) fileName
 
 main :: IO ()
 main = do
@@ -64,8 +85,3 @@ main = do
     case action of
         "save" -> save $ actargs
         "open" -> open $ actargs
-    putStrLn $ show args
-    d <- (eitherDecode <$> getJSON) :: IO (Either String [File])
-    case d of
-        Left err -> putStrLn err
-        Right f -> print f
